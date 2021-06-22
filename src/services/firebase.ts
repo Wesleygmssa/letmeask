b9657b945574419ce0132cc8bef892d8344b3c7c
Wesley Guerra <guerra_ssa@hotmail.com>
2021-06-21 22:44:47 -0300

    Finalizada conexão com firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,16 +3,16 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqeoetVWrfVDU6-rMsJrJKtniQplld6TM",
-  authDomain: "letmeask-e3667.firebaseapp.com",
-  databaseURL: "https://letmeask-e3667-default-rtdb.firebaseio.com",
-  projectId: "letmeask-e3667",
-  storageBucket: "letmeask-e3667.appspot.com",
-  messagingSenderId: "660726978500",
-  appId: "1:660726978500:web:c9eefcd27a8bbce5cb3691",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUT_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.analytics();
+export const auth = firebase.auth();
 export const database = firebase.database();
