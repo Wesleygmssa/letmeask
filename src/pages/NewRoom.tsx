92227e5f1b00d511342b17590f33d5b5b25556c5
Wesley Guerra <guerra_ssa@hotmail.com>
2021-06-25 20:38:12 -0300

    Criando sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -2,7 +2,7 @@ import illustrationimg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { FormEvent, useState } from "react";
 import { database } from "../services/firebase";
@@ -11,6 +11,7 @@ import { database } from "../services/firebase";
 
 export function NewRoom() {
   const { user } = useAuth();
+  const history = useHistory();
   const [newRoom, setNewRoom] = useState("");
 
   async function handleCreateRoom(event: FormEvent) {
@@ -26,6 +27,8 @@ export function NewRoom() {
       title: newRoom,
       authorId: user?.id,
     });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
   }
 
   return (
