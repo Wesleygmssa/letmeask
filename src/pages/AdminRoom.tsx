e7ef46aa9a62403938652317f8b3cb04e5c658db
Wesley Guerra <guerra_ssa@hotmail.com>
2021-06-29 07:35:09 -0300

    Criando página administrativa

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -18,7 +18,7 @@ type RoomParams = {
 
 const notify = () => toast.error("You must be logged in.");
 
-export function Room() {
+export function AdinRoom() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
@@ -74,31 +74,6 @@ export function Room() {
           <h1>Sala {title}</h1>
           {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
-
-        <form onSubmit={handleSendQuestion}>
-          <textarea
-            placeholder="O que você quer perguntar?"
-            onChange={(event) => setNewQuestion(event.target.value)}
-            value={newQuestion}
-          />
-
-          <div className="form-footer">
-            {user ? (
-              <div className="user-info">
-                <img src={user.avatar} alt={user.name} />
-                <span>{user.name}</span>
-              </div>
-            ) : (
-              <span>
-                Para enviar uma pergunta, <button>faça seu login</button>.
-              </span>
-            )}
-
-            <Button type="submit" disabled={!user}>
-              Enviar pergunta
-            </Button>
-          </div>
-        </form>
 
         <div className="question-list">
           {questions.map((question) => {
